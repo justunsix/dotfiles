3b25e41a2cab781bab05c8a512a6609924cc42d0
Justin Tung <justin.tung@live.ca>
2023-03-28 13:48:33 -0400

    fix html pasting into terminal

--- usr/bin/jt-clip-html-to-org.sh
+++ usr/bin/jt-clip-html-to-org.sh
@@ -3,6 +3,8 @@
 # Copy HTML from clipboard and convert to Emacs org format and set back to clipboard
 # for X, if using Wayland use wl-copy instead of xclip
 # Switches:
+# -selection : fix pasting html to terminal
+# per https://askubuntu.com/questions/1397056/work-around-for-xclip-o-selection-clipboard-t-text-html-error-target-t
 # --wrap=none : don't wrap lines
 # sed... : remove backslashes from line indicators added by pandoc
-xclip -o -t text/html | pandoc -f html -t org --wrap=none | sed 's/\\\\//g' | xclip -selection clipboard
+xclip -o -selection clipboard -t text/html | pandoc -f html -t org --wrap=none | sed 's/\\\\//g' | xclip -selection clipboard
