f7fcb1a608fe805e4901a59a31d337bedf1449f4
Justin Tung <justin.tung@live.ca>
2023-03-28 22:11:33 -0400

    remove use-package from minimal set up

--- .config/emacs/setup/minimal.el
+++ .config/emacs/setup/minimal.el
@@ -138,22 +138,18 @@
 ;; Splitting Windows
 ;; The Emacs default split doesn't seem too intuitive for most users.
 ;; Focus on the split window after splitting.
-(use-package emacs
-  :ensure nil
-  :preface
-  (defun ian/split-and-follow-horizontally ()
-    "Split window below."
-    (interactive)
-    (split-window-below)
-    (other-window 1))
-  (defun ian/split-and-follow-vertically ()
-    "Split window right."
-    (interactive)
-    (split-window-right)
-    (other-window 1))
-  :config
-  (global-set-key (kbd "C-x 2") #'ian/split-and-follow-horizontally)
-  (global-set-key (kbd "C-x 3") #'ian/split-and-follow-vertically))
+(defun ian/split-and-follow-horizontally ()
+  "Split window below."
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+(defun ian/split-and-follow-vertically ()
+  "Split window right."
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") #'ian/split-and-follow-horizontally)
+(global-set-key (kbd "C-x 3") #'ian/split-and-follow-vertically)
 
 ;; Reduce the matching parenthese highlight delay to instantly.
 (setq show-paren-delay 0)
