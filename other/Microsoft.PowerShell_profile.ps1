747f26330fa5860c7e2c5d1e5bee7e131aab6ba2
Justin <justin.tung@live.ca>
2023-04-28 04:49:12 -0400

    activate keys for PSFzf

--- other/Microsoft.PowerShell_profile.ps1
+++ other/Microsoft.PowerShell_profile.ps1
@@ -14,3 +14,13 @@ Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
 })
+
+# PSReadline
+Import-Module PSReadLine
+
+# PSFzf 
+Import-Module PSFzf
+# Select Current Provider Path (default chord: Ctrl+t)
+# Reverse Search Through PSReadline History (default chord: Ctrl+r)
+# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
