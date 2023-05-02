f8dc995617b7cc8af0e6d95159ccba574b710754
Justin Tung <justin.tung@live.ca>
2023-05-02 00:10:56 -0400

    PowerShell based git s check

---
+++ usr/bin-windows/jgt.ps1
@@ -0,0 +1,28 @@
+# Check folders at folder ~/Code and run git status in each folder
+# to find which folders have changes
+# Check if folder is a git repository, if so, run git status, otherwise skip
+# the folder
+# Run git status and check output,
+# if output contains "nothing to commit, working tree clean", then echo "."
+# else print the working directory
+
+# Get list of folders in ~/Code
+$folders = Get-ChildItem -Path ~/Code -Directory
+
+foreach ($folder in $folders) {
+    # Check if folder is a git repository
+    if (Test-Path -Path "$($folder.FullName)\.git") {
+        # Run git status
+        $git_status = git -C $folder.FullName status
+        # Check git status output
+        if ($git_status -match "nothing to commit, working tree clean") {
+            # If output contains "nothing to commit, working tree clean"
+            # then echo "."
+            Write-Host "." -NoNewline
+        } else {
+            # If output does not contain "nothing to commit, working tree clean"
+            # then print the working directory
+            Write-Host $folder.FullName "has changes"
+        }
+    }
+}
