292fddad4a9e64f49dcb107d0152f65966ff12b7
Justin Tung <justin.tung@live.ca>
2023-06-08 13:47:21 -0400

    nvim config: temporarily comment out import of custom plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -202,7 +202,7 @@ require('lazy').setup({
   --    up-to-date with whatever is in the kickstart repo.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  { import = 'custom.plugins' },
+  -- temporarily remove until there are custom plugins { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
