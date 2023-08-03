71d7b01b0f1a69f84c6db19096dc54f92c5b4c74
Justin Tung <justin.tung@live.ca>
2023-08-03 17:34:07 -0400

    check commands before updates

--- usr/bin/jt-update-all.sh
+++ usr/bin/jt-update-all.sh
@@ -14,60 +14,75 @@ if [ -f /etc/fedora-release ]; then
 		sudo dnf autoremove
 else
     # In Ubuntu
-    echo ' '
-    echo '----------------------------------------'
-    echo 'Updating APT packages'
-    echo ' '
-    sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-    sudo aptitude safe-upgrade -y
+		if command -v apt >/dev/null; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating APT packages'
+				echo ' '
+				sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+				sudo aptitude safe-upgrade -y
+    fi
 
     # Update all Snaps
-    echo ' '
-    echo '----------------------------------------'
-    echo 'Updating Snaps'
-    echo ' '
-    sudo snap refresh
+		if command -v snap >/dev/null; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating Snaps'
+				echo ' '
+				sudo snap refresh
+		fi
 
     # update all deb-gets
-    echo ' '
-    echo '----------------------------------------'
-    echo 'Updating deb-gets'
-    echo ' '
-    deb-get update && deb-get upgrade
+		if command -v deb-get >/dev/null; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating deb-gets'
+				echo ' '
+				deb-get update && deb-get upgrade
+		fi
 fi
 
 # Update all Flatpaks
-echo ' '
-echo '----------------------------------------'
-echo 'Updating Flatpaks'
-echo ' '
-sudo flatpak update -y
+if command -v flatpak >/dev/null; then
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Updating Flatpaks'
+		echo ' '
+		sudo flatpak update -y
+fi
 
 # update main pip installed packages
 # avoid upgrading all pip packages due to possible dependency issues
-echo ' '
-echo '----------------------------------------'
-echo 'Updating pip packages'
-echo ' '
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade ansible
-python3 -m pip install --upgrade tldr
+# Check if python3 and pip are installed
+if command -v python3 >/dev/null; then
+		if command -v pip >/dev/null; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating pip packages'
+				echo ' '
+				python3 -m pip install --upgrade pip
+				python3 -m pip install --upgrade ansible
+				python3 -m pip install --upgrade tldr
+		fi
+fi
 
 # Update all Nix packages
-echo ' '
-echo '----------------------------------------'
-echo 'Updating Nix packages'
-echo ' '
-nix-channel --update
-nix-env -u
+if [ -e "$HOME/.nix-profile/" ]; then
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Updating Nix packages'
+		echo ' '
+		nix-channel --update
+		nix-env -u
 
-# Run nix package manager garbage collection
-# delete generations older than 30 days
-nix-collect-garbage --delete-older-than 30d
-# If day is Friday, Saturday or Sunday ($(date +%u) -ge 5)
-# If date is 1st of month or 20th or month
-# if [ $(date +%d) = 01 ] || [ $(date +%d) = 20 ]; then
-# fi
+		# Run nix package manager garbage collection
+		# delete generations older than 30 days
+		nix-collect-garbage --delete-older-than 30d
+		# If day is Friday, Saturday or Sunday ($(date +%u) -ge 5)
+		# If date is 1st of month or 20th or month
+		# if [ $(date +%d) = 01 ] || [ $(date +%d) = 20 ]; then
+		# fi
+fi
 
 # Scan font directories to build font cache files
 # in case nix managed fonts were updated
