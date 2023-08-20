475c95a8daf7bb64e523c176b9dfe806c1197359
Justin Tung <justin.tung@live.ca>
2023-08-20 11:19:31 -0400

    occasional updates

--- usr/bin/jt-update-all.sh
+++ usr/bin/jt-update-all.sh
@@ -5,15 +5,15 @@
 # Check if Linux Distribution is Fedora
 if [ -f /etc/fedora-release ]; then
 
-    echo ' '
-    echo '----------------------------------------'
-    echo 'Updating DNF packages'
-    echo ' '
-    sudo dnf check-update
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Updating DNF packages'
+		echo ' '
+		sudo dnf check-update
 		sudo dnf upgrade -y
 		sudo dnf autoremove
 else
-    # In Ubuntu
+		# In Ubuntu
 		if command -v apt >/dev/null; then
 				echo ' '
 				echo '----------------------------------------'
@@ -21,9 +21,9 @@ else
 				echo ' '
 				sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 				sudo aptitude safe-upgrade -y
-    fi
+		fi
 
-    # Update all Snaps
+		# Update all Snaps
 		if command -v snap >/dev/null; then
 				echo ' '
 				echo '----------------------------------------'
@@ -32,7 +32,7 @@ else
 				sudo snap refresh
 		fi
 
-    # update all deb-gets
+		# update all deb-gets
 		if command -v deb-get >/dev/null; then
 				echo ' '
 				echo '----------------------------------------'
@@ -74,67 +74,76 @@ if [ -e "$HOME/.nix-profile/" ]; then
 		echo ' '
 		nix-channel --update
 		nix-env -u
-
-		# Run nix package manager garbage collection
-		# delete generations older than 30 days
-		nix-collect-garbage --delete-older-than 30d
-		# If day is Friday, Saturday or Sunday ($(date +%u) -ge 5)
-		# If date is 1st of month or 20th or month
-		# if [ $(date +%d) = 01 ] || [ $(date +%d) = 20 ]; then
-		# fi
-fi
-
-# Scan font directories to build font cache files
-# in case nix managed fonts were updated
-echo ' '
-echo '----------------------------------------'
-echo 'Updating font cache'
-echo ' '
-fc-cache -v
-
-echo ' '
-echo '----------------------------------------'
-echo 'Update nodejs version'
-echo ' '
-# nvm is a bash function, not a builtin, file or alias
-# from https://github.com/branneman/dotfiles/blob/master/scripts/updates
-if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
-		NVM_DIR="$HOME/.nvm"
-		source "$NVM_DIR/nvm.sh"
-		nvm install lts/*
-		nvm alias default lts/*
-		nvm use default
-fi
-
-# Update Clam, Freshclam sigantures
-# if ClamAV is installed
-if [ -f /usr/bin/clamscan ]; then
-		echo ' '
-		echo '----------------------------------------'
-		echo 'Updating ClamAV'
-		echo ' '
-		sudo freshclam
-fi
-
-# Update conda and its packages in base environment
-# if conda is installed
-if command -v conda >/dev/null; then
-        echo ' '
-        echo '----------------------------------------'
-        echo 'Updating conda'
-        echo ' '
-        conda update -n base conda -c anaconda --yes
-        conda update --all --yes
-				# clean unused packages
-				conda clean --all --yes
 fi
 
 # Update git repositories using topgrade
 # if topgrade is installed
 if command -v topgrade >/dev/null; then
-        echo ' '
-        echo '----------------------------------------'
-        echo 'Updating git repositories'
-        echo ' '
-        topgrade -y --only git_repos
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Updating git repositories'
+		echo ' '
+		topgrade -y --only git_repos
+fi
+
+# Updates to be run only occasionally
+# If day is Saturday or Sunday
+if [ "$(date +%u)" -ge 5 ]; then
+
+		# Scan font directories to build font cache files
+		# in case nix managed fonts were updated
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Updating font cache'
+		echo ' '
+		fc-cache -v
+
+		echo ' '
+		echo '----------------------------------------'
+		echo 'Update nodejs version'
+		echo ' '
+		# nvm is a bash function, not a builtin, file or alias
+		# from https://github.com/branneman/dotfiles/blob/master/scripts/updates
+		if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
+				NVM_DIR="$HOME/.nvm"
+				source "$NVM_DIR/nvm.sh"
+				nvm install lts/*
+				nvm alias default lts/*
+				nvm use default
+		fi
+
+		# Update Clam, Freshclam sigantures
+		# if ClamAV is installed
+		if [ -f /usr/bin/clamscan ]; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating ClamAV'
+				echo ' '
+				sudo freshclam
+		fi
+
+		# Update conda and its packages in base environment
+		# if conda is installed
+		if command -v conda >/dev/null; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Updating conda'
+				echo ' '
+				conda update -n base conda -c anaconda --yes
+				conda update --all --yes
+				# clean unused packages
+				conda clean --all --yes
+		fi
+
+		# Clean Nix packages
+		if [ -e "$HOME/.nix-profile/" ]; then
+				echo ' '
+				echo '----------------------------------------'
+				echo 'Cleaning Nix packages'
+				echo ' '
+				# Run nix package manager garbage collection
+				# delete generations older than 30 days
+				nix-collect-garbage --delete-older-than 30d
+		fi
+
 fi
