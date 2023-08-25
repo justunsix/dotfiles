5bbd4d160a420c2c177b953d5073ba8e7aec5bec
Justin Tung <justin.tung@live.ca>
2023-08-25 08:53:27 -0400

    emacs: csv-mode and functions

--- .config/emacs/setup/jt-emacs-languages.el
+++ .config/emacs/setup/jt-emacs-languages.el
@@ -136,6 +136,34 @@
 (use-package powershell)
 ;;	:mode "\\.ps1\\'")
 
+;; csv
+;; Recommendations from https://www.emacswiki.org/emacs/CsvMode
+(require 'cl)
+(require 'color)
+
+(defun csv-highlight (&optional separator)
+  (interactive (list (when current-prefix-arg (read-char "Separator: "))))
+  (font-lock-mode 1)
+  (let* ((separator (or separator ?\,))
+         (n (count-matches (string separator) (point-at-bol) (point-at-eol)))
+         (colors (loop for i from 0 to 1.0 by (/ 2.0 n)
+                       collect (apply #'color-rgb-to-hex
+                                      (color-hsl-to-rgb i 0.3 0.5)))))
+    (loop for i from 2 to n by 2
+          for c in colors
+          for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+          do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
+
+(use-package csv-mode
+	:mode "\\.csv\\'"
+	:config
+	(add-hook 'csv-mode-hook 'csv-highlight)
+	;; csv-align-mode helps with aligning columns on a set width
+  ;; (add-hook 'csv-mode-hook 'csv-highlight)
+	;; truncate long lines in a buffer
+	;; (add-hook 'csv-mode-hook '(lambda () (interactive) (toggle-truncate-lines nil))))
+	)
+
 (provide 'jt-emacs-languages)
 
 ;;; jt-emacs-languages.el ends here
