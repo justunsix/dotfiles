d80f5717f15f6f4efeb6568fae85ec40314fc83b
Justin Tung <justin.tung@live.ca>
2023-08-28 20:25:23 -0400

    emacs: csv colouring - flycheck and deprecation corrections

--- .config/emacs/setup/jt-emacs-languages.el
+++ .config/emacs/setup/jt-emacs-languages.el
@@ -9,6 +9,7 @@
 
 ; Breadcrumb to show structure of file and where you are located
 (defun efs/lsp-mode-setup ()
+	"Setup for lsp mode."
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode)
 	)
@@ -70,6 +71,7 @@
 ;; Defer LSP to start later, activate visual line (word wrap)
 ;; and remove line numbers
 (defun efs/markdown-mode-setup ()
+	"Setup for markdown mode."
 	(lsp-deferred)
 	(visual-line-mode 1)
 	(setq display-line-numbers nil)
@@ -144,20 +146,23 @@
 
 ;; csv
 ;; Recommendations from https://www.emacswiki.org/emacs/CsvMode
-(require 'cl)
+;; updated to Emacs 29.1
+
 (require 'color)
 
 (defun csv-highlight (&optional separator)
+	"Highlight CSV columns using with different colors.
+SEPARATOR is the character to use as a separator."
   (interactive (list (when current-prefix-arg (read-char "Separator: "))))
   (font-lock-mode 1)
   (let* ((separator (or separator ?\,))
-         (n (count-matches (string separator) (point-at-bol) (point-at-eol)))
-         (colors (loop for i from 0 to 1.0 by (/ 2.0 n)
-                       collect (apply #'color-rgb-to-hex
-                                      (color-hsl-to-rgb i 0.3 0.5)))))
-    (loop for i from 2 to n by 2
-          for c in colors
-          for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+         (n (count-matches (string separator) (line-beginning-position) (line-end-position)))
+         (colors (cl-loop for i from 0 to 1.0 by (/ 2.0 n)
+													collect (apply #'color-rgb-to-hex
+																				 (color-hsl-to-rgb i 0.3 0.5)))))
+    (cl-loop for i from 2 to n by 2
+						 for c in colors
+						 for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
           do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
 
 (use-package csv-mode
