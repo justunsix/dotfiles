d341f86d38631e900d51e69fe0f3a4c9ec206bb1
Justin Tung <justin.tung@live.ca>
2023-09-18 09:27:43 -0400

    Windows update: check if users wants topgrade

--- usr/bin-windows/jt-update-all.ps1
+++ usr/bin-windows/jt-update-all.ps1
@@ -2,7 +2,16 @@
 
 # Update with topgrade
 # disable system updates on Windows for now
-topgrade -y --disable system
+# Prompt user whether to run topgrade
+$runTopgrade = Read-Host "Do you want to run topgrade? (y/n)"
+
+# If user wants to run topgrade, then run topgrade with options
+if ($runTopgrade -eq "y") {
+    Write-Host "`nRunning topgrade" -ForegroundColor Green
+    topgrade -y --disable system
+}
+
+# topgrade -y --disable system
 
 Write-Host "`nCleaning scoop packages" -ForegroundColor Green
 scoop cleanup *
