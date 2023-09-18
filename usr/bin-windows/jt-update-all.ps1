420d59bc7c7654913dda66a2a491fee888526a4d
Justin Tung <justin.tung@live.ca>
2023-09-17 23:25:05 -0400

    fix typo

---
+++ usr/bin-windows/jt-update-all.ps1
@@ -0,0 +1,12 @@
+# Script to update all packages on a Windows machine
+
+# Update with topgrade
+# disable system updates on Windows for now
+# topgrade -y --disable system
+
+Write-Host "`nCleaning scoop packages" -ForegroundColor Green
+scoop cleanup *
+
+Write-Host "`nUpdating Emacs packages" -ForegroundColor Green
+emacs --batch --eval '(progn (package-refresh-contents) (package-upgrade-all))'
+emacs --batch -l ~/.config/emacs/setup/jt-emacs-package-managers.el --eval '(straight-pull-all)'
