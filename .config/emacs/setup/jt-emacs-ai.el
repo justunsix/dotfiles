cb4a79ccc519fd53c00665443213057389ed5551
Justin Tung <justin.tung@live.ca>
2023-12-22 10:13:26 -0500

    emacs: copilot suppress max warnings

--- .config/emacs/setup/jt-emacs-ai.el
+++ .config/emacs/setup/jt-emacs-ai.el
@@ -56,10 +56,15 @@
 (setq copilot-idle-delay 0.4)
 
 ;; Suppress warnings when a buffer exceeds the variable copilot-max-char
-;; suggested at https://github.com/zerolfx/copilot.el/pull/198
-;; and configured per https://github.com/blahgeek/emacs.d/blob/master/init.el
 (require 'warnings)
-(add-to-list 'warning-suppress-types '(copilot copilot-exceeds-max-char))
+;;;; suggested at https://github.com/zerolfx/copilot.el/pull/198
+;;;; and configured per https://github.com/blahgeek/emacs.d/blob/master/init.el
+(setq warning-suppress-types
+        '((emacs)
+          (copilot copilot-exceeds-max-char)))
+;;;; Having issues with using add-to-list to add to warning-suppress-types for some reason
+;;;; (add-to-list 'warning-suppress-types '((emacs)
+;;;;                 (copilot copilot-exceeds-max-char)))
 
 ;; Set copilot-overlay-face to inherit from font-lock-comment-face
 ;; Default is `shadow` which is too light
