df1efefd25e243beb65df06631288ea00ef03c92
Justin Tung <justin.tung@live.ca>
2024-01-28 21:16:54 -0500

    clean up: use kondo, linting

--- usr/bin/jt-clean-up.sh
+++ usr/bin/jt-clean-up.sh
@@ -8,35 +8,23 @@ if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
 		echo 'Clean unused nvm versions'
 		NVM_DIR="$HOME/.nvm"
 		source "$NVM_DIR/nvm.sh"
-		cd ~/.nvm/versions/node; ls -A | grep -v `nvm current` | xargs rm -rf
-fi
-
-# node_modules
-if [ -d "$HOME/Code" ]; then
-
-		echo 'Clean node_modules folders from projects, displaying impacted folders if any:'
-		cd ~/Code
-		find . -name "node_modules" -type d -prune | xargs du -chs
-		find . -name "node_modules" -type d -prune -exec rm -rf '{}' +
+		cd ~/.nvm/versions/node || exit; ls -A | grep -v 'nvm current' | xargs rm -rf
 fi
 
 # python venvs
 if [ -d "$HOME/Code" ]; then
 
 		echo 'Clean python venvs folders from projects, displaying impacted folders if any:'
-		cd ~/Code
+		cd "$HOME/Code" || exit
 		find . -name "venv" -type d -prune | xargs du -chs
 		find . -name "venv" -type d -prune -exec rm -rf '{}' +
-fi
 
-# Rust targets
-if [ -d "$HOME/.cargo" ]; then
-		if [ -d "$HOME/Code" ]; then
-			  echo 'Clean Rust targets'
-				cd ~/Code
-				find . -name "target" -type d -prune | xargs du -chs
-				find . -name "target" -type d -prune -exec rm -rf '{}' +
+		# kondo command exists
+		if command -v konda >/dev/null; then
+				echo 'Clean software projects unneeded files'
+				cd "$HOME/Code" && kondo
 		fi
+
 fi
 
 # Clean Docker images
