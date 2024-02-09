2c036aa4eba2fefef08e57f1c525dbab38784693
Justin Tung <justin.tung@live.ca>
2024-02-08 19:05:47 -0500

    Clean up, organize powershell profiles

--- other/PowerShell/Microsoft.PowerShell_profile.ps1
+++ other/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,36 +1,41 @@
 # PowerShell 7+ PROFILE
+# Edit like notepad $PROFILE
 # Stored in $PROFILE variable
 # Managed by Ansible
-# Edit like notepad $PROFILE
-Invoke-Expression (&starship init powershell)
 
-# Zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
+# Prompt
 
-# Aliases
-## Emacs
-## Uses function instead of alias
-## per https://stackoverflow.com/questions/4166370/how-can-i-write-a-powershell-alias-with-arguments-in-the-middle
-## function emacsnw {emacs -Q -nw -l ~\.config\emacs\setup\minimal.el $args}
+## Starship
+if (Get-Command "starship.exe" -ErrorAction SilentlyContinue)
+{
+		Invoke-Expression (&starship init powershell)
+}
 
-# PSReadline
+# Shell Support
+## Zoxide
+if (Get-Command "zoxide.exe" -ErrorAction SilentlyContinue)
+{
+		Invoke-Expression (& {
+													 $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+													 (zoxide init --hook $hook powershell | Out-String)
+											 })
+}
+
+## PSReadline
 Import-Module PSReadLine
-# Enable Predictive IntelliSense
+## Enable Predictive IntelliSense
 Set-PSReadLineOption -PredictionSource History
 
-# PSFzf
+## PSFzf
 Import-Module PSFzf
-# Select Current Provider Path (default chord: Ctrl+t)
-# Reverse Search Through PSReadline History (default chord: Ctrl+r)
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+## Select Current Provider Path (default chord: Ctrl+t)
+## Reverse Search Through PSReadline History (default chord: Ctrl+r)
+## replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Intellisense
+## Intellisense
 Import-Module CompletionPredictor
-## List view of predictions under the cursor
+### List view of predictions under the cursor
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 ## Azure Predictor
@@ -38,8 +43,18 @@ Import-Module Az.Tools.Predictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Enable-AzPredictor -AllSession
 
-# Broot
-# if Broot present, source function
-if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
-    . $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
+# Aliases
+## Emacs
+## Uses function instead of alias
+## per https://stackoverflow.com/questions/4166370/how-can-i-write-a-powershell-alias-with-arguments-in-the-middle
+## function emacsnw {emacs -Q -nw -l ~\.config\emacs\setup\minimal.el $args}
+
+# Integrated Programs
+
+## Broot
+if (Get-Command "broot.exe" -ErrorAction SilentlyContinue)
+{
+		if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
+				. $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
+		}
 }
--- other/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ other/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,7 @@
 # Windows PowerShell 5.1 PROFILE
+# Edit like notepad $PROFILE
 # Stored in $PROFILE variable
 # Managed by Ansible
-# Edit like notepad $PROFILE
-Invoke-Expression (&starship init powershell)
 
 # Chocolately
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -10,25 +9,33 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-# Zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
+# Simplify powershell.exe to be close to -noprofile
+$loadAll = $false
 
-# PSReadline
-Import-Module PSReadLine
-# Enable Predictive IntelliSense
-Set-PSReadLineOption -PredictionSource History
+if ($loadAll) {
 
-# PSFzf
-Import-Module PSFzf
-# Select Current Provider Path (default chord: Ctrl+t)
-# Reverse Search Through PSReadline History (default chord: Ctrl+r)
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+		Invoke-Expression (&starship init powershell)
 
-# if Broot present, source function
-if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
-    . $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
+		# Zoxide
+		Invoke-Expression (& {
+													 $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+													 (zoxide init --hook $hook powershell | Out-String)
+											 })
+
+		# PSReadline
+		Import-Module PSReadLine
+		# Enable Predictive IntelliSense
+		Set-PSReadLineOption -PredictionSource History
+
+		# PSFzf
+		Import-Module PSFzf
+		# Select Current Provider Path (default chord: Ctrl+t)
+		# Reverse Search Through PSReadline History (default chord: Ctrl+r)
+		# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+		Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+		# if Broot present, source function
+		if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
+				. $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
+		}
 }
