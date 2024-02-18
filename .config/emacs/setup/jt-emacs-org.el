cad157b90aaf129c097bca8ba019ea8b8ff096cf
Justin Tung <justin.tung@live.ca>
2024-02-17 20:02:42 -0500

    emacs: create new org notes

--- .config/emacs/setup/jt-emacs-org.el
+++ .config/emacs/setup/jt-emacs-org.el
@@ -259,6 +259,27 @@
 	(kill-new (format "[[file:%s]]" (file-name-nondirectory (buffer-file-name))))
 	)
 
+(defun jt/create-org-note (title)
+  "Create a new org file in jt/org-directory, add an ID, prompt user for title, filetags."
+  (interactive "sEnter the title of the org file: ")
+  (let* ((filename (concat (file-name-as-directory jt/org-directory) (replace-regexp-in-string " " "-" title) ".org"))
+         (buffer (find-file filename))
+         (tags (read-string "Enter tags separated by colon: "))
+				 (id-string (concat ":PROPERTIES:\n:ID: " (org-id-get-create) "\n:END:\n"))
+         (title-string (concat "#+title: " title "\n"))
+         (tags-string (concat "#+filetags: " tags "\n\n* See Also\n\n"))
+				 (file-link-string (concat "- [[file:" filename "][" title "]] - "))
+				 )
+    (with-current-buffer buffer
+      (insert id-string)
+      (insert title-string)
+      (insert tags-string)
+			(insert file-link-string)
+			)
+    (save-buffer)
+		)
+	)
+
 (defun jt/org-boldify-region ()
   "Boldify the selected region in Org mode or unbold if already."
   (interactive)
