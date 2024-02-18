408b9b4c2019a42fecf26b2bf82c938dbe11cc5e
Justin Tung <justin.tung@live.ca>
2024-02-18 17:02:37 -0500

    update: script, command detection and output

--- usr/bin/jt-clean-up.sh
+++ usr/bin/jt-clean-up.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+source "$(dirname "$0")/common.sh"
+
 # Clean up extra personal files such as:
 # - Old nvm nodejs version
 
 # nvm is a bash function, not a builtin, file or alias
 if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
-		echo 'Clean unused nvm versions'
+		write_host_with_timestamp "Clean unused nvm versions"
 		NVM_DIR="$HOME/.nvm"
 		source "$NVM_DIR/nvm.sh"
 		cd ~/.nvm/versions/node; ls -A | grep -v `nvm current` | xargs rm -rf
@@ -14,14 +16,14 @@ fi
 # python venvs
 if [ -d "$HOME/Code" ]; then
 
-		echo 'Clean python venvs folders from projects, displaying impacted folders if any:'
+		write_host_with_timestamp "Clean python virtual environments"
 		cd "$HOME/Code" || exit
 		find . -name "venv" -type d -prune | xargs du -chs
 		find . -name "venv" -type d -prune -exec rm -rf '{}' +
 
 		# kondo command exists
 		if command -v kondo >/dev/null; then
-				echo 'Clean software projects unneeded files'
+				write_host_with_timestamp "Clean software projects unneeded files"
 				cd "$HOME/Code" && kondo
 		fi
 
@@ -31,7 +33,7 @@ fi
 if command -v docker >/dev/null; then
 		# check Docker daemon is running
 		if docker info >/dev/null 2>&1; then
-				echo 'Clean Docker images'
+				write_host_with_timestamp "Clean Docker images"
 				docker system prune -a
 		else
    			echo 'Docker daemon is not running'
@@ -40,12 +42,12 @@ fi
 
 # Clean screenshots
 if [ -d "$HOME/Pictures/Screenshots" ]; then
-		echo 'Clean screenshots'
+		write_host_with_timestamp "Clean screenshots"
 		rm -rf ~/Pictures/Screenshots/*
 fi
 
 # Clean mpv watch information
 if [ -d "$HOME/.config/mpv/watch_later" ]; then
-		echo 'Clean mpv watch information'
+		write_host_with_timestamp "Clean mpv watch information"
 		rm -rf ~/.config/mpv/watch_later/*
 fi
--- usr/bin/jt-update-all.sh
+++ usr/bin/jt-update-all.sh
@@ -1,166 +1,145 @@
 #!/usr/bin/env bash
 
-source ~/usr/bin/common.sh
+source "$(dirname "$0")/common.sh"
 
 # Update all packages on the system
 
-# Check if Linux Distribution is Fedora
-if [ -f /etc/fedora-release ]; then
-
-		write_host_with_timestamp "Updating DNF packages"
-		sudo dnf check-update
-		sudo dnf upgrade -y
-		sudo dnf autoremove
+# Fedora packages
+if command -v dnf >/dev/null; then
+	write_host_with_timestamp "Updating DNF packages"
+	sudo dnf check-update
+	sudo dnf upgrade -y
+	sudo dnf autoremove
 fi
 
-# If Linux distribution is Arch
-if [ -f /etc/arch-release ]; then
-		write_host_with_timestamp "Updating pacman packages"
-		sudo pacman -Syu --noconfirm
+# Arch packages
+if command -v pacman >/dev/null; then
+	write_host_with_timestamp "Updating pacman packages"
+	sudo pacman -Syu --noconfirm
 fi
 
-# If Linux distribution is Ubuntu, set isUbuntu variable to "true"
-if [ -f /etc/os-release ]; then
-    # Get os-release variables
-    . /etc/os-release
-    if [ "$ID" = "ubuntu" ]; then
-
-				# In Ubuntu
-				if command -v apt >/dev/null; then
-						write_host_with_timestamp "Updating APT packages"
-						sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-						sudo aptitude safe-upgrade -y
-				fi
-
-				# Update all Snaps
-				if command -v snap >/dev/null; then
-						write_host_with_timestamp "Updating Snaps"
-						sudo snap refresh
-				fi
-
-    fi
+# Debian, Ubuntu apt packages
+if command -v apt >/dev/null; then
+	write_host_with_timestamp "Updating APT packages"
+	sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+	sudo aptitude safe-upgrade -y
 fi
 
-# Detect MacOS
-if [[ "$OSTYPE" == "darwin"* ]]; then
-		isMacOS="true"
+# Snap packages
+if command -v snap >/dev/null; then
+	write_host_with_timestamp "Updating Snaps"
+	sudo snap refresh
+fi
 
-		# Update Homebrew
-		if command -v brew >/dev/null; then
-				write_host_with_timestamp "Updating Homebrew packages"
-				echo ' '
-				brew update && brew upgrade && brew cleanup
-		fi
-
+# Homebrew packages
+if command -v brew >/dev/null; then
+	write_host_with_timestamp "Updating Homebrew packages"
+	brew update && brew upgrade && brew cleanup
 fi
 
 # Update git repositories using topgrade
 # if topgrade is installed
 if command -v topgrade >/dev/null; then
-		write_host_with_timestamp "Updating git repositories"
-		topgrade -y --only git_repos
+	write_host_with_timestamp "Updating git repositories"
+	topgrade -y --only git_repos
 fi
 
 if [ -f "$HOME/.config/home-manager/flake.nix" ]; then
-		write_host_with_timestamp "Updating Nix Flake and Home Manager"
-		cd "$HOME/.config/home-manager/" || exit
-    nix flake update && home-manager switch -b bak
-		cd - || exit
+	write_host_with_timestamp "Updating Nix Flake and Home Manager"
+	cd "$HOME/.config/home-manager/" || exit
+	nix flake update && home-manager switch -b bak
+	cd - || exit
 fi
 
 # Update all Flatpaks
 if command -v flatpak >/dev/null; then
-		write_host_with_timestamp "Updating Flatpaks"
-		echo ' '
-		sudo flatpak update -y
+	write_host_with_timestamp "Updating Flatpaks"
+	sudo flatpak update -y
 fi
 
 # update main pip installed packages
 # avoid upgrading all pip packages due to possible dependency issues
 # unless pipx is installed
 if command -v python3 >/dev/null; then
-		if command -v pipx >/dev/null; then
-				write_host_with_timestamp "Updating pipx packages"
-				pipx upgrade-all
-		else
-				if command -v pip >/dev/null; then
-						write_host_with_timestamp "Updating pipx packages"
-						python3 -m pip install --user --upgrade pip
-						python3 -m pip install --user --upgrade ansible
-						python3 -m pip install --user --upgrade tldr
-				fi
+	if command -v pipx >/dev/null; then
+		write_host_with_timestamp "Updating pipx packages"
+		pipx upgrade-all
+	else
+		if command -v pip >/dev/null; then
+			write_host_with_timestamp "Updating pipx packages"
+			python3 -m pip install --user --upgrade pip
+			python3 -m pip install --user --upgrade ansible
+			python3 -m pip install --user --upgrade tldr
 		fi
+	fi
 fi
 
 # Update all Nix packages
 if [ -e "$HOME/.nix-profile/" ] || [ -e "/nix/var/nix/profiles/" ]; then
-		write_host_with_timestamp "Updating Nix packages"
-		nix-channel --update
-		nix-env -u
-		if [ -x "$(command -v home-manager)" ]; then
-				home-manager switch
-		fi
+	write_host_with_timestamp "Updating Nix packages"
+	nix-channel --update
+	nix-env -u
+	if [ -x "$(command -v home-manager)" ]; then
+		home-manager switch
+	fi
 fi
 
 # If day is Saturday or Sunday
 # or if first argument was true
 if [ "$(date +%u)" -ge 5 ] || [ "$1" = "true" ]; then
 
-    # Update Emacs Packages
-		if command -v emacs >/dev/null; then
-				write_host_with_timestamp "Updating Emacs packages"
-				emacs --batch -l ~/.config/emacs/setup/jt-emacs-package-managers.el --eval '(auto-package-update-now)'
-				emacs --batch -l ~/.config/emacs/setup/jt-emacs-package-managers.el --eval '(straight-pull-all)'
-		fi
+	# Update Emacs Packages
+	if command -v emacs >/dev/null; then
+		write_host_with_timestamp "Updating Emacs packages"
+		emacs --batch -l ~/.config/emacs/setup/jt-emacs-package-managers.el --eval '(auto-package-update-now)'
+		emacs --batch -l ~/.config/emacs/setup/jt-emacs-package-managers.el --eval '(straight-pull-all)'
+	fi
 
-		# update all deb-gets
-		if command -v deb-get >/dev/null; then
-				write_host_with_timestamp "Updating deb-gets"
-				deb-get update && deb-get upgrade
-		fi
+	# update all deb-gets
+	if command -v deb-get >/dev/null; then
+		write_host_with_timestamp "Updating deb-gets"
+		deb-get update && deb-get upgrade
+	fi
 
-		# Scan font directories to build font cache files
-		# in case nix managed fonts were updated
-		echo ' '
-		echo '----------------------------------------'
-		echo 'Updating font cache'
-		echo ' '
-		fc-cache -v
+	# Scan font directories to build font cache files
+	# in case nix managed fonts were updated
+	write_host_with_timestamp "Updating font cache"
+	fc-cache -v
 
-		# nvm is a bash function, not a builtin, file or alias
-		# from https://github.com/branneman/dotfiles/blob/master/scripts/updates
-		if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
-				write_host_with_timestamp "Update nodejs version in nvm"
-				NVM_DIR="$HOME/.nvm"
-				source "$NVM_DIR/nvm.sh"
-				nvm install lts/*
-				nvm alias default lts/*
-				nvm use default
-		fi
+	# nvm is a bash function, not a builtin, file or alias
+	# from https://github.com/branneman/dotfiles/blob/master/scripts/updates
+	if [ -d "$HOME/.nvm" ] && [ -s "$HOME/.nvm/nvm.sh" ]; then
+		write_host_with_timestamp "Update nodejs version in nvm"
+		NVM_DIR="$HOME/.nvm"
+		source "$NVM_DIR/nvm.sh"
+		nvm install lts/*
+		nvm alias default lts/*
+		nvm use default
+	fi
 
-		# Update Clam, Freshclam sigantures
-		# if ClamAV is installed
-		if [ -f /usr/bin/clamscan ]; then
-				write_host_with_timestamp "Updating ClamAV"
-				sudo freshclam
-		fi
+	# Update Clam, Freshclam sigantures
+	# if ClamAV is installed
+	if [ -f /usr/bin/clamscan ]; then
+		write_host_with_timestamp "Updating ClamAV"
+		sudo freshclam
+	fi
 
-		# Update conda and its packages in base environment
-		# if conda is installed
-		if command -v conda >/dev/null; then
-				write_host_with_timestamp "Updating conda"
-				conda update -n base conda -c anaconda --yes
-				conda update --all --yes
-				# clean unused packages
-				conda clean --all --yes
-		fi
+	# Update conda and its packages in base environment
+	# if conda is installed
+	if command -v conda >/dev/null; then
+		write_host_with_timestamp "Updating conda"
+		conda update -n base conda -c anaconda --yes
+		conda update --all --yes
+		# clean unused packages
+		conda clean --all --yes
+	fi
 
-		# Clean Nix packages
-		if [ -e "$HOME/.nix-profile/" ] || [ -e "/nix/var/nix/profiles/" ]; then
-				write_host_with_timestamp "Cleaning Nix packages"
-				# Run nix package manager garbage collection
-				# delete generations older than 30 days
-				nix-collect-garbage --delete-older-than 30d
-		fi
+	# Clean Nix packages
+	if [ -e "$HOME/.nix-profile/" ] || [ -e "/nix/var/nix/profiles/" ]; then
+		write_host_with_timestamp "Cleaning Nix packages"
+		# Run nix package manager garbage collection
+		# delete generations older than 30 days
+		nix-collect-garbage --delete-older-than 30d
+	fi
 
 fi
