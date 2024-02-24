c1233337006737460d1efa3934156a56e1cab6a2
Justin Tung <justin.tung@live.ca>
2024-02-24 12:35:09 -0500

    pwsh: organize file, add completions

--- other/PowerShell/Microsoft.PowerShell_profile.ps1
+++ other/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,47 +4,59 @@
 # Managed by Ansible
 
 # Variables
-$env:EDITOR = "nvim"
+$env:EDITOR = "hx"
 
 # Prompt
 
 ## Starship
-if (Get-Command "starship.exe" -ErrorAction SilentlyContinue)
-{
-		Invoke-Expression (&starship init powershell)
+if (Get-Command "starship.exe" -ErrorAction SilentlyContinue) {
+	Invoke-Expression (&starship init powershell)
 }
 
 # Shell Support
-## Zoxide
-if (Get-Command "zoxide.exe" -ErrorAction SilentlyContinue)
-{
-		Invoke-Expression (& {
-													 $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-													 (zoxide init --hook $hook powershell | Out-String)
-											 })
-}
-
 ## PSReadline
 Import-Module PSReadLine
-## Enable Predictive IntelliSense
-Set-PSReadLineOption -PredictionSource History
+Import-Module CompletionPredictor
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+### List view of predictions under the cursor
+### Set-PSReadLineOption -PredictionViewStyle ListView
+
+## Zoxide
+if (Get-Command "zoxide.exe" -ErrorAction SilentlyContinue) {
+	Invoke-Expression (& {
+			$hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+													 (zoxide init --hook $hook powershell | Out-String)
+		})
+}
+
+## Carapace - Shell completions
+if (Get-Command "carapace.exe" -ErrorAction SilentlyContinue) {
+	$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
+	Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+	Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+	carapace _carapace | Out-String | Invoke-Expression
+}
+
+## Broot - File explorer, directory navigator
+if (Get-Command "broot.exe" -ErrorAction SilentlyContinue) {
+	if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
+		. $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
+	}
+}
 
 ## PSFzf
+## https://github.com/kelleyma49/PSFzf
 Import-Module PSFzf
 ## Select Current Provider Path (default chord: Ctrl+t)
 ## Reverse Search Through PSReadline History (default chord: Ctrl+r)
 ## replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-## Intellisense
-Import-Module CompletionPredictor
-### List view of predictions under the cursor
-Set-PSReadLineOption -PredictionViewStyle ListView
+# Intellisense
 
 ## Azure Predictor
-Import-Module Az.Tools.Predictor
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Enable-AzPredictor -AllSession
+### Import-Module Az.Tools.Predictor
+### Enable-AzPredictor -AllSession
 
 # Aliases
 ## Emacs
@@ -52,12 +64,3 @@ Enable-AzPredictor -AllSession
 ## per https://stackoverflow.com/questions/4166370/how-can-i-write-a-powershell-alias-with-arguments-in-the-middle
 ## function emacsnw {emacs -Q -nw -l ~\.config\emacs\setup\minimal.el $args}
 
-# Integrated Programs
-
-## Broot
-if (Get-Command "broot.exe" -ErrorAction SilentlyContinue)
-{
-		if (Test-Path $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1) {
-				. $env:USERPROFILE\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
-		}
-}
