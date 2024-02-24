03821c685fdb4b8dceded967966b914c86a3bb40
Justin Tung <justin.tung@live.ca>
2024-02-24 09:39:22 -0500

    emacs: fix calling yas before it loaded

--- .config/emacs/setup/jt-emacs-languages.el
+++ .config/emacs/setup/jt-emacs-languages.el
@@ -29,12 +29,11 @@
 (use-package yasnippet
 	:hook (lsp-mode . yas-minor-mode)
 	:config
+	;; Activate yasnippet globally
+	;; can be done on per buffer with yas-minor-mode
+	;; or M-x yas-reload-all if you've started YASnippet already.
+	(yas-global-mode 1)
 	)
-
-;; Activate yasnippet globally
-;; can be done on per buffer with yas-minor-mode
-;; or M-x yas-reload-all if you've started YASnippet already.
-(yas-global-mode 1)
 
 ;; Install snippet collection
 (use-package yasnippet-snippets
