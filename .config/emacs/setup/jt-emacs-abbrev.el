c80e560fc17720f149b5055a9adfa86c89b20c43
Justin Tung <justin.tung@live.ca>
2024-02-24 18:51:22 -0500

    emacs: abbrev using modified Teeline

--- .config/emacs/setup/jt-emacs-abbrev.el
+++ .config/emacs/setup/jt-emacs-abbrev.el
@@ -5,13 +5,32 @@
 ;;; Code:
 
 ;;-*-coding: utf-8;-*-
-;; Started from http://xahlee.info/emacs/emacs/emacs_abbrev_mode_tutorial.html
+;; Follow modified Teeline shorthand for typed text
+;; https://www.wikihow.com/Write-Shorthand#Teeline-Shorthand
+;; Stage 1.
+;; - Leave out 
+;;   - Middle vowels (AEIOU)
+;;   - Double vowels
+;;   - Double consonants
+;; - Keep vowels at the beginning and end of a word
+;; Stage 2.
+;; - Exclude silent letters
+;; - Use brief forms for common words
+;; - Use abbreviations
 (define-abbrev-table 'global-abbrev-table
   '(
     ;; English words
-    ("zbg" "background" nil :count 0)
-    ;; -- IT
-    ("zdb" "database" nil :count 0)
+    ("bckgrnd" "background" nil :count 0)
+    ("dtbse" "database" nil :count 0)
+
+    ;; -- Abbreviations
+    ("OPP" "opportunity" nil :count 0)
+
+    ;; -- Common words
+    ("z+" "and" nil :count 0)
+    ("zr" "are" nil :count 0)
+    ("/w" "with" nil :count 0)
+    ("ur" "your" nil :count 0)
    ))
 
 
