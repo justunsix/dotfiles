e216d77317eb99e0ef3b3b9973df2592a6fb02b7
Justin Tung <justin.tung@live.ca>
2024-02-23 22:12:15 -0500

    changes defaults

--- .config/emacs/setup/variables.el
+++ .config/emacs/setup/variables.el
@@ -4,10 +4,10 @@
 ;; Override with your settings
 
 ;; Location of your org files
-(defvar jt/org-directory (concat (getenv "HOME") "/org"))
+(defvar jt/org-directory (concat (getenv "HOME") "/Code"))
 
 ;; Directory containing projects, can be list of paths like ("~/projects" "~/work")
-(defvar jt/project-search-path (concat (getenv "HOME") "/Code"))
+(defvar jt/project-search-path '(concat (getenv "HOME") "/Code"))
 
 ;; Location of dotfiles
 (defvar jt/dotfiles-dir (concat (getenv "HOME") "/Code/dotfiles"))
