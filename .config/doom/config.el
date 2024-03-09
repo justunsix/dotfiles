1ac6864f6afa417ec314de0b0a4b606b9f0e9e68
Justin Tung <justin.tung@live.ca>
2024-03-08 20:44:15 -0500

    emacs: small improvements

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Justin Tung"
+      user-mail-address "")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -58,15 +58,6 @@
 ;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
 ;;   - Setting doom variables (which start with 'doom-' or '+').
 ;;
-(after! which-key
-  (setq which-key-idle-delay 0.2)
-  )
-
-(after! yasnippet-snippets
-  ;; Add (yasnippet backquote-change) to ‘warning-suppress-types’
-  ;; to turn off warnings of elisp executions in snippets
-  (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
-  )
 
 ;; Here are some additional functions/macros that will help you configure Doom.
 ;;
@@ -86,6 +77,25 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; --------------------------------------------------------------------------------
+;; * Package Configurations  ----------------------------
+
+(after! which-key
+  (setq which-key-idle-delay 0.2)
+  )
+
+(after! yasnippet-snippets
+  ;; Add (yasnippet backquote-change) to ‘warning-suppress-types’
+  ;; to turn off warnings of elisp executions in snippets
+  (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
+  )
+
+;; --------------------------------------------------------------------------------
+;; * Other Variables  ----------------------------
+
+;; Do not prompt on exit
+(setq confirm-kill-emacs nil)
 
 ;; --------------------------------------------------------------------------------
 ;; * Overlay  ----------------------------
