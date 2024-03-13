f373ef71c9984f53e801f2676436b66e3912977e
Justin Tung <justin.tung@live.ca>
2024-03-12 23:39:36 -0400

    comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,7 +35,7 @@ if type -q oc
 end
 
 if type -q carapace
-  # https://rsteube.github.io/carapace-bin/setup.html
+	# https://carapace-sh.github.io/carapace-bin/setup.html
 	set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
 	mkdir -p ~/.config/fish/completions
 	carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
