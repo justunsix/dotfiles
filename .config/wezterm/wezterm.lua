83d2dca1a12e12ef3ee1367f525101ab91ff2c9a
Justin Tung <justin.tung@live.ca>
2024-04-12 15:50:28 -0400

    wezterm: add Cygwin shell in Windows

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -36,6 +36,8 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 	 })
 	 table.insert(config.launch_menu, { label = 'cmd', args = {'cmd.exe'},
 	 })
+	 table.insert(config.launch_menu, { label = 'Cygwin Shell', args = {'cygwin-shell.bat'},
+	 })
 
 
 end
