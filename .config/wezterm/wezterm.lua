dd555b9e74a2e93a5e81238ab0c2676046c39e3f
Justin Tung <justin.tung@live.ca>
2024-04-25 15:18:51 -0400

    wezterm: save session history to file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -76,6 +76,51 @@ config.keys = {
 
 }
 
+-- Saving scrollback and pane
+-- from: https://wezfurlong.org/wezterm/config/lua/pane/get_lines_as_text.html
+
+local io = require 'io'
+local os = require 'os'
+local act = wezterm.action
+
+wezterm.on('trigger-vim-with-scrollback', function(window, pane)
+  -- Retrieve the text from the pane
+  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+  -- Create a temporary file to pass to vim
+  local name = os.tmpname()
+  local f = io.open(name, 'w+')
+  f:write(text)
+  f:flush()
+  f:close()
+
+  -- Open a new window running vim and tell it to open the file
+  -- window:perform_action(
+  --   act.SpawnCommandInNewWindow {
+  --     args = { 'vim', name },
+  --   },
+  --   pane
+  -- )	
+
+  -- output the filename's location to the current active pane
+  pane:inject_output('Session saved to: ' .. name)
+
+  -- Wait "enough" time for vim to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous wrt. running
+  -- this script and are not awaitable, so we just pick a number.
+  --
+  -- Note: We don't strictly need to remove this file, but it is nice
+  -- to avoid cluttering up the temporary directory.
+  -- wezterm.sleep_ms(1000)
+  -- os.remove(name)
+end)
+
+-- Merge the keys table with the existing one
+table.insert(config.keys, {
+  key = 'E',
+  mods = 'CTRL',
+  action = act.EmitEvent 'trigger-vim-with-scrollback',
+})
 
 -- ====================================================
 -- and finally, return the configuration to wezterm
