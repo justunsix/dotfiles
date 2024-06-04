708a1443bb1ee6c7e6ef4d2caf0d2b108bb9e438
Justin Tung <justin.tung@live.ca>
2024-06-04 19:28:56 -0400

    emacs: activate roam more

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -160,7 +160,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org                ; organize your plain life in plain text
-        ;;+roam2
+        +roam2
         )
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
