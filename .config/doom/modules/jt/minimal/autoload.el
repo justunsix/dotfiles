8af4fd51bd2a7ca7d2b66ff46d0a0ee22dd5a8d7
Justin Tung <justin.tung@live.ca>
2024-09-10 10:10:50 -0400

    emacs: presentation mode ui toggle

--- .config/doom/modules/jt/minimal/autoload.el
+++ .config/doom/modules/jt/minimal/autoload.el
@@ -43,3 +43,28 @@
       (sort-lines nil (point-min) (point-max)))
     (save-buffer))  
   )
+
+(defvar jt-org-presentation-mode nil
+  "Non-nil if presentation mode is enabled.")
+
+;;;###autoload
+(defun jt/org-toggle-presentation-mode ()
+  "Toggle presentation mode settings."
+  (interactive)
+  (if jt-org-presentation-mode
+      (progn
+        ;; Revert to default settings
+        (setq doom-font (font-spec :family "Jetbrains Mono" :size 15)):want
+        (load-theme 'doom-tokyo-night t)
+        (display-line-numbers-mode 1)
+        (doom/reload-font)        
+        (setq jt-org-presentation-mode nil)
+        (message "Presentation mode disabled"))
+    (progn
+      ;; Apply presentation settings
+      (setq doom-font (font-spec :family "Jetbrains Mono" :size 15))
+      (load-theme 'doom-plain t)
+      (display-line-numbers-mode -1)
+      (doom/reload-font)
+      (setq jt-org-presentation-mode t)
+      (message "Presentation mode enabled"))))
