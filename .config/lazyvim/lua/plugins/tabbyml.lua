89e878a9c706d1505ad12f043b85efcec5046053
Justin Tung <justin.tung@live.ca>
2024-10-03 11:27:30 -0400

    nvim: add tabby ai

---
+++ .config/lazyvim/lua/plugins/tabbyml.lua
@@ -0,0 +1,7 @@
+-- Use other key instead of Tab as workaround to LazyVim conflicts
+-- https://github.com/TabbyML/tabby/issues/862
+vim.g.tabby_keybinding_accept = "C-CR"
+
+return {
+  { "TabbyML/vim-tabby" },
+}
