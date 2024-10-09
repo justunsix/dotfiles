cff9992909352b2e47a2d551dd23d86beef216f5
Justin Tung <justin.tung@live.ca>
2024-10-09 11:19:41 -0400

    nvim: org enable meta return in insert

--- .config/lazyvim/lua/plugins/org-mode.lua
+++ .config/lazyvim/lua/plugins/org-mode.lua
@@ -1,15 +1,20 @@
 return {
 
-  {
-    "nvim-orgmode/orgmode",
-    event = "VeryLazy",
-    ft = { "org" },
-    config = function()
-      -- Setup orgmode
-      require("orgmode").setup({
-        org_agenda_files = "~/orgfiles/**/*",
-        org_default_notes_file = "~/orgfiles/refile.org",
-      })
-    end,
-  },
+	{
+		"nvim-orgmode/orgmode",
+		event = "VeryLazy",
+		ft = { "org" },
+		config = function()
+			-- Setup orgmode
+			require("orgmode").setup({
+				org_agenda_files = "~/orgfiles/**/*",
+				org_default_notes_file = "~/orgfiles/refile.org",
+				-- Use Enter in insert mode to add list items/checkboxes/todos
+				-- https://github.com/nvim-orgmode/orgmode/blob/nightly/DOCS.md#use-enter-in-insert-mode-to-add-list-itemscheckboxestodos
+				mappings = {
+					org_return_uses_meta_return = true,
+				},
+			})
+		end,
+	},
 }
