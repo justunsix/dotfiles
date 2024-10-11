201ecaf642547bb9444caee9391d3ff5fdc4bee0
Justin Tung <justin.tung@live.ca>
2024-10-11 10:03:18 -0400

    nvim: org, go back to using default org meta return

--- .config/lazyvim/lua/plugins/org-mode.lua
+++ .config/lazyvim/lua/plugins/org-mode.lua
@@ -1,15 +1,15 @@
 -- Map Shift + Enter to org meta return
 -- https://github.com/nvim-orgmode/orgmode/blob/nightly/DOCS.md#use-enter-in-insert-mode-to-add-list-itemscheckboxestodos
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "org",
-	callback = function()
-		vim.keymap.set("i", "<S-CR>", '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
-			silent = true,
-			buffer = true,
-		})
-	end,
-})
-
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "org",
+-- 	callback = function()
+-- 		vim.keymap.set("i", "<S-CR>", '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+-- 			silent = true,
+-- 			buffer = true,
+-- 		})
+-- 	end,
+-- })
+--
 return {
 
 	{
@@ -21,6 +21,11 @@ return {
 			require("orgmode").setup({
 				org_agenda_files = "~/orgfiles/**/*",
 				org_default_notes_file = "~/orgfiles/refile.org",
+				-- Enter in insert mode will always do org meta return
+				-- per fix in above comments
+				mappings = {
+					org_return_uses_meta_return = true,
+				},
 			})
 		end,
 	},
