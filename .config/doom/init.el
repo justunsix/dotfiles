782e74e2dd9beeab44f956e859db8a0236e08e5c
Justin Tung <justin.tung@live.ca>
2024-10-12 00:50:07 -0400

    emacs: doom rust and shell support

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -171,10 +171,10 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp +tree-sitter)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       (sh +powershell)    ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +fish +lsp +powershell +tree-sitter)    ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
