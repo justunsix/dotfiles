280a2fe2d923745574d1795c47fc4f4a8f37572b
Justin Tung <justin.tung@live.ca>
2024-10-14 16:55:38 -0400

    emacs: terraform lsp support

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -106,7 +106,7 @@
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       terraform         ; infrastructure as code
+       (terraform +lsp)         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
