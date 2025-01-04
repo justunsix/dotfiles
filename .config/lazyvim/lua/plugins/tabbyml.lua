aa9ee5a51ded09605a5e394ad32b4f92636057af
Justin Tung <justin.tung@live.ca>
2025-01-04 15:19:20 -0500

    chore(nvim): remove unused plugins

--- .config/lazyvim/lua/plugins/tabbyml.lua
+++
@@ -1,7 +0,0 @@
--- Use other key instead of Tab as workaround to LazyVim conflicts
--- https://github.com/TabbyML/tabby/issues/862
-vim.g.tabby_keybinding_accept = "C-CR"
-
-return {
-  { "TabbyML/vim-tabby" },
-}
