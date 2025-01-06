2bdc91954e43747a8bbe67cd5ae81d24d66dc1a4
Justin Tung <justin.tung@live.ca>
2025-01-05 21:21:41 -0500

    feat(emacs): replace fn with :sort, formatting

--- .config/doom/modules/jt/minimal/autoload.el
+++ .config/doom/modules/jt/minimal/autoload.el
@@ -5,23 +5,23 @@
 	title, filetags. Filename will be the title with spaces replaced by hyphens."
   (interactive "sEnter the title of the org file: ")
   (let*
-	((filename (concat (replace-regexp-in-string " " "-" title) ".org"))
-         (buffer (find-file filename))
-         (tags (read-string "Enter tags separated by colon: "))
-				 (id-string (concat ":PROPERTIES:\n:ID: " (org-id-get-create) "\n:END:\n"))
-         (title-string (concat "#+title: " title "\n"))
-         (tags-string (concat "#+filetags: " tags "\n\n* See Also\n\n"))
-				 (file-link-string (concat "- [[file:" filename "][" title "]] - "))
-				 )
+      ((filename (concat (replace-regexp-in-string " " "-" title) ".org"))
+       (buffer (find-file filename))
+       (tags (read-string "Enter tags separated by colon: "))
+       (id-string (concat ":PROPERTIES:\n:ID: " (org-id-get-create) "\n:END:\n"))
+       (title-string (concat "#+title: " title "\n"))
+       (tags-string (concat "#+filetags: " tags "\n\n* See Also\n\n"))
+       (file-link-string (concat "- [[file:" filename "][" title "]] - "))
+       )
     (with-current-buffer buffer
       (insert id-string)
       (insert title-string)
       (insert tags-string)
-			(insert file-link-string)
-			)
+      (insert file-link-string)
+      )
     (save-buffer)
-		)
-	)
+    )
+  )
 
 ;; Get name of current file without full path and copy to kill ring
 ;;;###autoload
@@ -31,17 +31,6 @@
   (let ((filename (file-name-nondirectory (buffer-file-name))))
     (kill-new filename)
     (message "Copied buffer file name '%s' to the clipboard." filename))
-    )
-
-;;;###autoload
-(defun jt/sort-lines-buffer ()
-  "Sort all lines in the current buffer and save changes."
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (widen)
-      (sort-lines nil (point-min) (point-max)))
-    (save-buffer))  
   )
 
 ;;;###autoload
