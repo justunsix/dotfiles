b559be6c2df568677c54ecbf36a8321d20c24b21
Justin Tung <justin.tung@live.ca>
2025-01-11 12:07:31 -0500

    feat(emacs): default org dir to temp

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -44,7 +44,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/.cache/org")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
