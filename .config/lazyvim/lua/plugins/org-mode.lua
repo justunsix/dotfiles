c6a550db1d2ea6360e1b14c2d222962941f1e30c
Justin Tung <justin.tung@live.ca>
2025-09-15 19:50:34 -0400

    feat(nvim): org meta return M-Ret

--- .config/lazyvim/lua/plugins/org-mode.lua
+++ .config/lazyvim/lua/plugins/org-mode.lua
@@ -1,15 +1,3 @@
--- Map Shift + Enter to org meta return
--- https://github.com/nvim-orgmode/orgmode/blob/nightly/DOCS.md#use-enter-in-insert-mode-to-add-list-itemscheckboxestodos
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "org",
--- 	callback = function()
--- 		vim.keymap.set("i", "<S-CR>", '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
--- 			silent = true,
--- 			buffer = true,
--- 		})
--- 	end,
--- })
---
 return {
 
 	{
@@ -19,16 +7,28 @@ return {
 		config = function()
 			-- Setup orgmode
 			require("orgmode").setup({
-				-- for folds on opening file, inherit from nvim's global foldlevel
+				-- For folds on opening file, inherit from nvim's global foldlevel
 				org_startup_folded = "inherit",
 				-- Indents are only visual and not saved in file
 				org_startup_indented = true,
 				org_adapt_indentation = false,
-				-- Enter in insert mode will always do org meta return
-				-- per fix in above comments
-				-- mappings = {
-				--  org_return_uses_meta_return = true,
-				-- },
+				-- Map Alt + Enter to org meta return
+				-- https://github.com/nvim-orgmode/orgmode/blob/master/docs/
+				-- configuration.org#use-enter-in-insert-mode-to-add-list-itemscheckboxestodos
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = "org",
+					callback = function()
+						vim.keymap.set(
+							"i",
+							"<M-CR>",
+							'<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>',
+							{
+								silent = true,
+								buffer = true,
+							}
+						)
+					end,
+				}),
 			})
 		end,
 	},
