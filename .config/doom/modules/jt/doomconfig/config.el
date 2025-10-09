faf85ea6a72310ea4ecd940f4315025a53749abc
Justin Tung <justin.tung@live.ca>
2025-10-08 20:25:12 -0400

    feat(emacs): require org-eww explicitly

--- .config/doom/modules/jt/doomconfig/config.el
+++ .config/doom/modules/jt/doomconfig/config.el
@@ -33,6 +33,9 @@
   ;; Set file types that will open in Emacs from org file links
   (add-to-list 'org-file-apps '("\\.puml\\'" . emacs))
 
+  ;; Included in org-mode core, need to activate for now 2025-10
+  (require 'ol-eww)
+
   )
 
 (after! plantuml-mode
