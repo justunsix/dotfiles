ecbf6663b68eecfdc365c8424bf459a32561e599
Justin <justin.tung@live.ca>
2025-10-10 10:31:25 -0400

    feat(emacs): doom add editorconfig

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -95,7 +95,7 @@
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
