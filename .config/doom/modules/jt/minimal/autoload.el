6889ba9940b42b806aea6a3ba1734839afb521df
Justin Tung <justin.tung@live.ca>
2026-03-29 09:21:37 -0400

    feat(emacs): find-dired fuzzy isearch

--- .config/doom/modules/jt/minimal/autoload.el
+++ .config/doom/modules/jt/minimal/autoload.el
@@ -1,8 +1,9 @@
 ;; from modules/jt/minimal/autoload.el
 ;;;###autoload
 (defun jt/create-org-note (title)
-  "Create a new org file in current directory, add an ID, prompt user for
-	title, filetags. Filename will be the title with spaces replaced by hyphens."
+  "Create new org file in current directory, add ID, and ask user for details.
+
+Details are title, filetags. Filename will be the title with spaces replaced by hyphens."
   (interactive "sEnter the title of the org file: ")
   (let*
       ((filename (concat (replace-regexp-in-string " " "-" title) ".org"))
@@ -23,7 +24,6 @@
     )
   )
 
-;; Get name of current file without full path and copy to kill ring
 ;;;###autoload
 (defun jt/copy-file-name-to-clipboard ()
   "Copy the current buffer file name to the kill ring (clipboard)."
@@ -32,3 +32,28 @@
     (kill-new filename)
     (message "Copied buffer file name '%s' to the clipboard." filename))
   )
+
+;;;###autoload
+(defun jt/find-dired-fuzzy (pattern)
+  "Run find-dired with fuzzy -iname matching for PATTERN.
+
+Takes the given PATTERN and inserts asterisks at the beginning and
+end and around spaces to enable fuzzy matching of filenames in the current directory."
+  (interactive "sEnter filename pattern: ")
+  (let* (
+         ;; Build fuzzy pattern: prepend/append asterisk, replace spaces with asterisk.
+         ;; Example: "foo bar" -> "*foo*bar*"
+         ;; The leading asterisk allows matching files that START with "foo".
+         ;; The trailing asterisk allows matching files that END with the last word.
+         ;; Replacing spaces with asterisks allows any characters between words.
+         (fuzzy-pattern (concat "*" (replace-regexp-in-string " " "*" pattern) "*"))
+         ;; Determine the directory to search:
+         ;; If already in dired-mode, use its directory (the buffer's default-directory).
+         ;; Otherwise, expand the current working directory to an absolute path.
+         (default-directory (if (derived-mode-p 'dired-mode)
+                                default-directory
+                              (expand-file-name default-directory))))
+    ;; let constrains find-dired to use -exec ls -ld {} + instead of -ls
+    ;; to remove escaped spaces in find-dired buffer (\ )
+    (let ((find-ls-option '("-exec ls -ld {} +" . "-ld")))
+      (find-dired default-directory (concat "-iname \"" fuzzy-pattern "\"")))))
