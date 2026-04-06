32a3bc211a2d945ac5221bfbc05f5ded55e685e6
Justin Tung <justin.tung@live.ca>
2026-04-06 19:22:01 -0400

    fix(emacs): video config

--- .config/doom/modules/jt/video/config.el
+++ .config/doom/modules/jt/video/config.el
@@ -1,9 +1,4 @@
 ;; from modules/jt/video/config.el
 (use-package! elfeed-tube-mpv
   :defer t
-  :commands elfeed
-  :config
-  :bind (:map elfeed-show-mode-map
-              ("C-c C-f" . elfeed-tube-mpv-follow-mode)
-              ("C-c C-w" . elfeed-tube-mpv-where)))
-)
+  :commands elfeed)
