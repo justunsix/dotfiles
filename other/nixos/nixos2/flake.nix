c6ececeebd00895b1214ea73c47f4f3448e7f66d
Justin Tung <justin.tung@live.ca>
2026-07-31 14:01:40 -0400

    fix(nix-flake): hm input, formatting

--- other/nixos/nixos2/flake.nix
+++ other/nixos/nixos2/flake.nix
@@ -13,83 +13,86 @@
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-26-05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixos-hardware,
-    ...
-  } @ inputs: let
-    # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    # # Your custom packages
-    # # Accessible through 'nix build', 'nix shell', etc
-    # packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # # Formatter for your nix files, available through 'nix fmt'
-    # # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    # formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-hardware,
+      ...
+    }@inputs:
+    let
+      # Supported systems for your flake packages, shell, etc.
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      # # Your custom packages
+      # # Accessible through 'nix build', 'nix shell', etc
+      # packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      # # Formatter for your nix files, available through 'nix fmt'
+      # # Other options beside 'alejandra' include 'nixpkgs-fmt'
+      # formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    # # Your custom packages and modifications, exported as overlays
-    # overlays = import ./overlays {inherit inputs;};
-    # # Reusable nixos modules you might want to export
-    # # These are usually stuff you would upstream into nixpkgs
-    # nixosModules = import ./modules/nixos;
-    # # Reusable home-manager modules you might want to export
-    # # These are usually stuff you would upstream into home-manager
-    # homeManagerModules = import ./modules/home-manager;
+      # # Your custom packages and modifications, exported as overlays
+      # overlays = import ./overlays {inherit inputs;};
+      # # Reusable nixos modules you might want to export
+      # # These are usually stuff you would upstream into nixpkgs
+      # nixosModules = import ./modules/nixos;
+      # # Reusable home-manager modules you might want to export
+      # # These are usually stuff you would upstream into home-manager
+      # homeManagerModules = import ./modules/home-manager;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # replace with your hostname
-      surface = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          # > Our main nixos configuration file <
-          ./hosts/surface/configuration.nix
-          # nixos-hardware.nixosModules.microsoft-surface-common
-        ];
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # replace with your hostname
+        surface = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            # > Our main nixos configuration file <
+            ./hosts/surface/configuration.nix
+            # nixos-hardware.nixosModules.microsoft-surface-common
+          ];
+        };
+        nixosbtw = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/nixosbtw/configuration.nix
+          ];
+        };
       };
-      nixosbtw = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/nixosbtw/configuration.nix
-        ];
+
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        # replace with your username@hostname
+        "justin@nixosbtw" = home-manager.lib.homeManagerConfiguration {
+          # Home-manager requires 'pkgs' instance
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # replace x86_64-linux with your architecture
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/home.nix
+          ];
+        };
       };
+
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # replace with your username@hostname
-      "justin@nixosbtw" = home-manager.lib.homeManagerConfiguration {
-        # Home-manager requires 'pkgs' instance
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # replace x86_64-linux with your architecture 
-        extraSpecialArgs = {inherit inputs;};
-        modules = [
-          # > Our main home-manager configuration file <
-          ./home-manager/home.nix
-        ];
-      };
-    };
-    
-  };
 }
