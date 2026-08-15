1dc16aae0ed37f7cbe7be7117ac94656147059e8
Justin Tung <justin.tung@live.ca>
2026-08-14 23:36:12 -0400

    feat(nixos): add libvirt and gnome-boxes

--- other/nixos/hosts/nixosmis/configuration.nix
+++ other/nixos/hosts/nixosmis/configuration.nix
@@ -74,6 +74,7 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "libvirtd"
     ];
     packages = with pkgs; [
       #  thunderbird
@@ -103,7 +104,19 @@
     git
     btop-rocm
     #  wget
+    gnome-boxes
   ];
+
+  # Virtualization, Virtual Machines
+  # https://wiki.nixos.org/wiki/Libvirt
+  virtualisation.libvirtd.enable = true;
+  # Enable TPM emulation (optional)
+  # install pkgs.swtpm system-wide for use in virt-manager (optional)
+  virtualisation.libvirtd.qemu = {
+    swtpm.enable = true;
+  };
+  # Enable USB redirection (optional)
+  virtualisation.spiceUSBRedirection.enable = true;
 
   programs.steam = {
     enable = true;
