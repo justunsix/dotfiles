615c786eb4c6470753c50da8327d8019eea3205a
Justin Tung <justin.tung@live.ca>
2026-08-16 16:39:45 -0400

    feat(nixos): input-remapper autoload

--- other/nixos/hosts/nixosmis/configuration.nix
+++ other/nixos/hosts/nixosmis/configuration.nix
@@ -165,6 +165,28 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  # Key binding service
+  # Autoload from https://github.com/thursdaddy/nixos-config/blob/f21380b188bd3941b32656e832c65111c437f463/modules/desktop/input-remapper.nix
+  services.input-remapper = {
+    enable = true;
+    serviceWantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.user.services.input-remapper-autoload = {
+    description = "Run input-remapper-control autoload command";
+    documentation = [ "https://github.com/sezanzeb/input-remapper" ];
+    after = [ "graphical-session.target" ];
+    bindsTo = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.input-remapper}/bin/input-remapper-control --command autoload";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      KillMode = "mixed";
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
