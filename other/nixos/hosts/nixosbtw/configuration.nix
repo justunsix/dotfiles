2984791ba78a2e67ad5a3c32f3c8a9b289703a6b
Justin Tung <justin.tung@live.ca>
2026-08-20 09:31:16 -0400

    fix(nixos): formatting

--- other/nixos/hosts/nixosbtw/configuration.nix
+++ other/nixos/hosts/nixosbtw/configuration.nix
@@ -178,7 +178,7 @@ in
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-    
+
     # Wayland requires kernel mode setting (KMS) to be enabled
     modesetting.enable = true;
 
@@ -206,15 +206,16 @@ in
     # package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
 
     # Temporary workaround for build failure 2026-08-18
-    # https://github.com/NixOS/nixpkgs/issues/554125   
-package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "595.91.07";
-    sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
-    sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
-    openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
-    settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
-    persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
-}  };
+    # https://github.com/NixOS/nixpkgs/issues/554125
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "595.91.07";
+      sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+      sha256_aarch64 = "sha256-fqkN7ONFXtTeXyu2mQxorrk362Epxq3bz88hhKYQzwQ=";
+      openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+      settingsSha256 = "sha256-QzT8Cw1luuZGP9DUje3HN/0ngiayqHURj+bqPsxlJ5w=";
+      persistencedSha256 = "sha256-3JQBaNmkwxvCXv9q8aHKas6VZM/JjLsuilC2t7ET0u0=";
+    };
+  };
 
   # Enable flakes https://nix.dev/concepts/flakes.html
   nix.settings.experimental-features = [
