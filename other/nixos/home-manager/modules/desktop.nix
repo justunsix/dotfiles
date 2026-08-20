2282a215c527ccbd1986b8ab8684a5bfe67c1e60
Justin Tung <justin.tung@live.ca>
2026-08-20 19:28:32 -0400

    feat(nix): disable udiskie, rely on nautilus

--- other/nixos/home-manager/modules/desktop.nix
+++ other/nixos/home-manager/modules/desktop.nix
@@ -13,17 +13,17 @@
   };
 
   # USB drive automounting
-  services.udiskie = {
-    enable = true;
-    settings = {
-      # workaround for
-      # https://github.com/nix-community/home-manager/issues/632
-      program_options = {
-        # replace with your favorite file manager
-        file_manager = "${pkgs.nautilus}/bin/nautilus";
-      };
-    };
-  };
+  # services.udiskie = {
+  #   enable = true;
+  #   settings = {
+  #     # workaround for
+  #     # https://github.com/nix-community/home-manager/issues/632
+  #     program_options = {
+  #       # replace with your favorite file manager
+  #       file_manager = "${pkgs.nautilus}/bin/nautilus";
+  #     };
+  #   };
+  # };
 
   # https://nix-community.github.io/home-manager/options/home-manager/services/gpg-agent.html#opt-services.gpg-agent.enable
   # https://tsawyer87.github.io/posts/gpg-agent_on_nixos/
