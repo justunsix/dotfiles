f6290cb658952bd5f16cb142027193befd4c8fa1
Justin Tung <justin.tung@live.ca>
2026-08-20 09:19:36 -0400

    feat(nixos): use Nvidia kernel drivers

--- other/nixos/hosts/nixosbtw/configuration.nix
+++ other/nixos/hosts/nixosbtw/configuration.nix
@@ -152,10 +152,14 @@ in
   # NVIDIA https://nixos.wiki/wiki/Nvidia
 
   # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-
+  hardware.graphics.enable = true;
+  # Use the NVidia open source kernel module (not to be confused with the
+  # independent third-party "nouveau" open source driver).
+  # Support is limited to the Turing and later architectures. Full list of
+  # supported GPUs is at:
+  # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  # Only available from driver 515.43.04+
+  hardware.nvidia.open = true;
   # Enable AMD ROCM or NVIDIA CUDA support for nixpkgs that have it like pkgs.btop
   # nixpkgs.config.rocmSupport = true;
   # CUDA Setup https://wiki.nixos.org/wiki/CUDA
@@ -175,7 +179,7 @@ in
 
   hardware.nvidia = {
 
-    # Modesetting is required.
+    # Wayland requires kernel mode setting (KMS) to be enabled
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
@@ -188,14 +192,6 @@ in
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = true;
-
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
@@ -207,7 +203,7 @@ in
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/nvidia-x11/default.nix
     # Try due to error, unknown if caused by 595 and other series drivers:
     # https://forums.developer.nvidia.com/t/nvidia-drm-error-flip-event-timeout-on-head-0-system-hangs-during-boot/374743
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    # package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 
   # Enable flakes https://nix.dev/concepts/flakes.html
