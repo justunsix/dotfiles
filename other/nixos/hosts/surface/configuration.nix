88a20c4938a088dbc51d0a0d4b3203d96505d1b3
Justin Tung <justin.tung@live.ca>
2026-08-23 10:06:12 -0400

    feat(nixos): add host btop

--- other/nixos/hosts/surface/configuration.nix
+++ other/nixos/hosts/surface/configuration.nix
@@ -148,6 +148,7 @@
     nushell
     gnumake
     stow
+    btop
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
