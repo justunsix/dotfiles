3f4e1f156734fe48787ca2adef823a17e1eb70aa
Justin Tung <justin.tung@live.ca>
2026-08-30 19:16:29 -0400

    feat(nixos): nautilus media information, other host

--- other/nixos/hosts/nixosbtw/configuration.nix
+++ other/nixos/hosts/nixosbtw/configuration.nix
@@ -243,6 +243,21 @@ in
     # pkgs-unstable.firefox
   ];
 
+  # Nautilus media information
+  # https://wiki.nixos.org/wiki/Nautilus
+  nixpkgs.overlays = [
+    (final: prev: {
+      nautilus = prev.nautilus.overrideAttrs (nprev: {
+        buildInputs =
+          nprev.buildInputs
+          ++ (with pkgs.gst_all_1; [
+            gst-plugins-good
+            gst-plugins-bad
+          ]);
+      });
+    })
+  ];
+
   # VirtualBox - https://wiki.nixos.org/wiki/VirtualBox
   # virtualisation = {
   #   virtualbox.host.enable = true;
